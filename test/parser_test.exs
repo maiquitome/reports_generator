939b36575d8b59328269f8d7d04545163ca5cdbc
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-06 06:00:38 -0300

    :white_check_mark: add parser_test

---
+++ test/parser_test.exs
@@ -0,0 +1,35 @@
+defmodule ReportsGenerator.ParserTest do
+  use ExUnit.Case
+  # doctest ReportsGenerator.ParserTest
+
+  alias ReportsGenerator.Parser
+
+  describe "parse_file/1" do
+    test "Extracts information from a file and parse lines." do
+      # SETUP
+      file_name = "report_test.csv"
+
+      # EXERCISE
+      response =
+        file_name
+        |> Parser.parse_file()
+        |> Enum.map(& &1)
+
+      expected_response = [
+        ["1", "pizza", 48],
+        ["2", "açaí", 45],
+        ["3", "hambúrguer", 31],
+        ["4", "esfirra", 42],
+        ["5", "hambúrguer", 49],
+        ["6", "esfirra", 18],
+        ["7", "pizza", 27],
+        ["8", "esfirra", 25],
+        ["9", "churrasco", 24],
+        ["10", "churrasco", 36]
+      ]
+
+      # ASSERTION
+      assert response == expected_response
+    end
+  end
+end
